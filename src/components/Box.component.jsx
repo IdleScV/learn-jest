277bbdbd77e49c3d0e217e880f781f75721b1f51
Wesley Chen <weschen.github@gmail.com>
2020-06-18 00:01:36 -0400

    Update Box.component.jsx

--- src/components/Box.component.jsx
+++ src/components/Box.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const add = (x, y) => x + y;
 
 export const total = (shipping, handling) => {
-	return `$ ${shipping + handling}`;
+	return `$ ${add(shipping, handling)}`;
 };
 
 export default function Box() {
